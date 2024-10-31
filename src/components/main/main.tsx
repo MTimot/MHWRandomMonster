9e88e04f66a5dcc0c8692e7be52e06c1ef9522cf
Kyky Gaming <mesmacquetimothey@gmail.com>
2024-10-31 16:09:45 +0100

    FINISHED

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,5 +1,21 @@
 import "./main.css";
-function Main() {
+interface monsterinterface {
+	monster: {
+		name: string;
+		iconsrc: string;
+	};
+}
+function Main({ monster, setMonsterIndex, weapon, setWeaponIndex }) {
+	function getRandomIntMonster() {
+		return Math.floor(Math.random() * 71);
+	}
+	function getRandomIntWeapon() {
+		return Math.floor(Math.random() * 14);
+	}
+	function handleClick() {
+		setMonsterIndex(getRandomIntMonster);
+		setWeaponIndex(getRandomIntWeapon);
+	}
 	return (
 		<>
 			<video
@@ -10,9 +26,23 @@ function Main() {
 				className="videomhw"
 			/>
 			<main>
-				<h1>On chasse ?</h1>
-				<div className="icon-placeholder"> </div>
-				<button type="button" id="chassonsbutton">
+				<h1>MONSTER RANDOMIZER</h1>
+				<h2>
+					{monster.name} with {weapon.name}
+				</h2>
+				<div className="bothicons">
+					<div className="icon-placeholder">
+						<img
+							className="monster-icon"
+							src={monster.iconsrc}
+							alt="CA MARCHE PAS"
+						/>
+					</div>
+					<div className="weapon-container">
+						<img className="weaponimg" src={weapon.iconsrc} alt={weapon.name} />
+					</div>
+				</div>
+				<button type="button" id="chassonsbutton" onClick={handleClick}>
 					Chassons!!
 				</button>
 			</main>
